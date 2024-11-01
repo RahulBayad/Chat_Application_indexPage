1791de449689ca498d26ab8b380e4a2f92918528
RahulBayad <rahulbayad05@gmail.com>
2024-11-01 18:19:03 +0530

    menu btn close btn error fixing

--- assests/js/theme.js
+++ assests/js/theme.js
@@ -1,12 +1,16 @@
 function openSidebar(){
     const menu = document.querySelector(".menu");
+    const closeBtn = document.querySelector("#close_icon");
     menu.style.transform = "translateX(0px)";
+    closeBtn.style.display = "inline-block"
 }
 function closeSidebar(){
     const menu = document.querySelector(".menu");
+    const closeBtn = document.querySelector("#close_icon");
     menu.style.transform = "translateX(-1000px)";
+    closeBtn.style.display = "none"
 }
 
-document.querySelectorAll(".menu-item").forEach(item =>{
-    item.addEventListener("click",closeSidebar);
-})
+// document.querySelectorAll(".menu-item").forEach(item =>{
+//     item.addEventListener("click",closeSidebar);
+// })
